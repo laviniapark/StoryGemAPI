55ed5720247a8d10590c3f0f30b4c49a1571d8ea
laviniapark <laviniasoo03@gmail.com>
2025-04-11 02:37:20 -0300

    Correcao da localizacao de uma anotacao

--- src/main/java/br/com/storygem/StorygemApplication.java
+++ src/main/java/br/com/storygem/StorygemApplication.java
@@ -2,8 +2,14 @@ package br.com.storygem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@EnableCaching
+@OpenAPIDefinition(info = @Info(title = "Story Gem", version = "v1", description = "Projeto WEB de criação de fichas"))
 public class StorygemApplication {
 
 	public static void main(String[] args) {
