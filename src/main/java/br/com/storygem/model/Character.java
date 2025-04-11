adee32bb4090730cf0e8d19355eeb70bb9ade250
laviniapark <laviniasoo03@gmail.com>
2025-04-11 02:10:26 -0300

    Implementaçao do database seeder

--- src/main/java/br/com/storygem/model/Character.java
+++ src/main/java/br/com/storygem/model/Character.java
@@ -4,9 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Data;
 
+
 @Data
+@Builder
 @Entity
 public class Character {
     
